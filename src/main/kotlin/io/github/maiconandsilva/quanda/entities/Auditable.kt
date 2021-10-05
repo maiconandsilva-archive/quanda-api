dc3ec32f77d7cef00bedaec117c68ed245b14955
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-10-05 07:27:19 -0300

    Delete unused classes/interfaces

--- src/main/kotlin/io/github/maiconandsilva/quanda/entities/Auditable.kt
+++
@@ -1,9 +0,0 @@
-package io.github.maiconandsilva.quanda.entities
-
-import java.util.Date
-import javax.persistence.*
-
-interface Auditable {
-    var createdDate: Date
-    var lastModifiedDate: Date
-}
